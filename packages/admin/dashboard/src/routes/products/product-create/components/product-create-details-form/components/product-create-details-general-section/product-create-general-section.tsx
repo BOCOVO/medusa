9564fd449a3eb9967a10df3f7985361107641ff7
bocovo <bocovojuste@gmail.com>
2024-11-04 20:29:32 +0100

    chore(app-admin): purge create product general info section
    Purge create product general info section. Only keep the title and description. Removed `handle`, `subtitle`.

--- packages/admin/dashboard/src/routes/products/product-create/components/product-create-details-form/components/product-create-details-general-section/product-create-general-section.tsx
+++ packages/admin/dashboard/src/routes/products/product-create/components/product-create-details-form/components/product-create-details-general-section/product-create-general-section.tsx
@@ -3,7 +3,6 @@ import { UseFormReturn } from "react-hook-form"
 import { useTranslation } from "react-i18next"
 
 import { Form } from "../../../../../../../components/common/form"
-import { HandleInput } from "../../../../../../../components/inputs/handle-input"
 import { ProductCreateSchemaType } from "../../../../types"
 
 type ProductCreateGeneralSectionProps = {
@@ -17,59 +16,21 @@ export const ProductCreateGeneralSection = ({
 
   return (
     <div id="general" className="flex flex-col gap-y-6">
-      <div className="flex flex-col gap-y-2">
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
-          <Form.Field
-            control={form.control}
-            name="title"
-            render={({ field }) => {
-              return (
-                <Form.Item>
-                  <Form.Label>{t("products.fields.title.label")}</Form.Label>
-                  <Form.Control>
-                    <Input {...field} placeholder="Winter jacket" />
-                  </Form.Control>
-                </Form.Item>
-              )
-            }}
-          />
-          <Form.Field
-            control={form.control}
-            name="subtitle"
-            render={({ field }) => {
-              return (
-                <Form.Item>
-                  <Form.Label optional>
-                    {t("products.fields.subtitle.label")}
-                  </Form.Label>
-                  <Form.Control>
-                    <Input {...field} placeholder="Warm and cosy" />
-                  </Form.Control>
-                </Form.Item>
-              )
-            }}
-          />
-          <Form.Field
-            control={form.control}
-            name="handle"
-            render={({ field }) => {
-              return (
-                <Form.Item>
-                  <Form.Label
-                    tooltip={t("products.fields.handle.tooltip")}
-                    optional
-                  >
-                    {t("fields.handle")}
-                  </Form.Label>
-                  <Form.Control>
-                    <HandleInput {...field} placeholder="winter-jacket" />
-                  </Form.Control>
-                </Form.Item>
-              )
-            }}
-          />
-        </div>
-      </div>
+      <Form.Field
+        control={form.control}
+        name="title"
+        render={({ field }) => {
+          return (
+            <Form.Item>
+              <Form.Label>{t("products.fields.title.label")}</Form.Label>
+              <Form.Control>
+                <Input {...field} placeholder="Winter jacket" />
+              </Form.Control>
+            </Form.Item>
+          )
+        }}
+      />
+
       <Form.Field
         control={form.control}
         name="description"
